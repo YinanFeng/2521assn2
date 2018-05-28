86e0c7e9c3d230f9ba714d13f5b4ce83f9095f7e
YinanFeng <36937743+YinanFeng@users.noreply.github.com>
2018-05-28 14:41:02 +1000

    Add files via upload
    update

--- Dijkstra.c
+++ Dijkstra.c
@@ -16,95 +16,188 @@ typedef struct ShortestPaths* Shortest;
 
 
 ShortestPaths dijkstra(Graph g, Vertex v){
-    Shortest shortest = calloc(1,sizeof(ShortestPaths));
+    
+    Shortest shortest = malloc(sizeof(ShortestPaths));
     shortest->noNodes = numVerticies(g);
     shortest->src = v;
-    PredNode* forcalloc;
-    shortest->pred = calloc(shortest->noNodes,sizeof(forcalloc));
-    
+    //PredNode* forcalloc;
+    shortest->dist = malloc(shortest->noNodes*sizeof(int));
+    shortest->pred = malloc(shortest->noNodes*sizeof(PredNode*));
     int i = 0;
     while(i < shortest->noNodes){
         shortest->dist[i] = infinity;
         shortest->pred[i] = NULL;
         i++;
     }
+
+    int* inthePQ = malloc((shortest->noNodes)*sizeof(int));
+    int h = 0;
+    while(h < shortest->noNodes){
+        inthePQ[h] = 0;
+        h++;
+    }
     
-    shortest->dist[v] = 0;
-    
-    //key de yong chu shi shen me?
-    int key = 1;
     
     PQ pq = newPQ();
-    ItemPQ first;
-    first.key = 1;
-    key = key+1;
-    first.value = v;
-    addPQ(pq,first);
     
-    ItemPQ temp;
+    ItemPQ first;
+    first.key = v;
+    first.value = 0;
+    addPQ(pq,first);
+    inthePQ[first.key] = 1;
+       //  temp = dequeuePQ(pq);
+    
     AdjList allOut;
-    while(PQEmpty(pq) == False){
-        //**pred is that kind of usage(pred[])
+    shortest->dist[v] = 0;
+   
+    while(!PQEmpty(pq)){
+      //  printf("here\n");
+        ItemPQ temp;
         temp = dequeuePQ(pq);
-        allOut = outIncident(g,temp.value);
+        inthePQ[temp.key] = 0;
+        
+    //    printf("here");
+      //  printf("%d\n",temp.key);
+    
+        allOut = outIncident(g,temp.key);
+   //     printf("%d\n",temp.key);
+  //   printf("%d\n",allOut->w);
+       // printf("once\n");
         while(allOut != NULL){
-            while(allOut->w != v){
+          //  printf("there\n");
+            PredNode* createNew = malloc(sizeof(PredNode));
+            if(allOut->w != v){
+
+            //    printf("bb\n");
+              //  printf("%d\n",allOut->w);
                 if(shortest->dist[allOut->w] == infinity){
-                    //right?
+                    //printf("aa\n");
                     ItemPQ newOne;
-                    newOne.key = key;
-                    key = key+1;
-                    newOne.value = allOut->w;
-                    addPQ(pq,newOne);
+                    newOne.key = allOut->w;
+                   // printf("keyyy %d\n",newOne.key);
+
+                    
+                    newOne.value = allOut->weight;
+                    
+              //     printf("%d\n",allOut->weight);
+                 //   printf("twice\n");
+                    
+                   //  printf("BeforeAdd\n");
+                   // showPQ(pq);
+                      addPQ(pq,newOne);
+                      inthePQ[allOut->w] = 1;
+                      
+                      
+                   // printf("AfterAdd\n");
+                   // showPQ(pq);
+                    
                 }
-            
-                if(shortest->dist[temp.value] + allOut->weight < shortest->dist[allOut->w]){
-                    PredNode* record = shortest->pred[allOut->w];
-                    PredNode* tempUse;
-                    while(record != NULL){
-                        tempUse = record->next;
-                        free(record);
-                        record = tempUse;
-                    }
-                    PredNode* createNew = calloc(1,sizeof(PredNode));
-                    createNew->v = temp.value;
+                //showPQ(pq);
+                if(shortest->dist[temp.key] + allOut->weight < shortest->dist[allOut->w]){
+                //    printf("goes here right \n");
+
+
+//                    printf("%d\n",temp.value);
+//                    printf("%d\n",shortest->dist[temp.value]);
+                   //   PredNode* record = shortest->pred[allOut->w];
+               //     PredNode* tempUse;
+                    
+//                    while(record != NULL){
+//                        tempUse = record->next;
+//                        free(record);
+//                        record = tempUse;
+//                    }
+                   
+                  
+                    //printf("ggg\n");
+           //         PredNode* createNew = malloc(sizeof(PredNode));
+                    
+                    //printf("ggg\n");
+                    
+                    createNew->v = temp.key;
+                    
+                    
+                    //printf("%d",createNew->v);
+            //shortest->pred[allOut->w] = malloc(sizeof(PredNode));
+              
                     shortest->pred[allOut->w] = createNew;
-                    shortest->dist[allOut->w] = shortest->dist[temp.value] + allOut->weight;
-                }else if(shortest->dist[temp.value] + allOut-> weight == shortest->dist[allOut->w]){
+                    shortest->dist[allOut->w] = shortest->dist[temp.key] + allOut->weight;
+                    if(inthePQ[allOut->w] == 0){
+                        ItemPQ theNewPQ;
+                        theNewPQ.key = allOut->w;
+                        theNewPQ.value = allOut->weight;
+                        addPQ(pq,theNewPQ);
+                        inthePQ[allOut->w] = 1;
+                    }
+                
+                    
+                    
+                }else if(shortest->dist[temp.key] + allOut-> weight == shortest->dist[allOut->w]){
+                    //printf("6\n");
+              
+                    
                     PredNode* record = shortest->pred[allOut->w];
                     //right?? record->next at first will not be record. do not need to consider record==null at first???
                     while(record->next != NULL){
                         record = record->next;
                     }
-                    PredNode* createNew = calloc(1,sizeof(PredNode));
-                    createNew->v = temp.value;
+             //       PredNode* createNew = malloc(sizeof(PredNode));
+                    createNew->v = temp.key;
                     record->next = createNew;
                 }
             }
             allOut = allOut->next;
         }
+        //mshowPQ(pq);
     }
-    
-    
+    int l = 0;
+    while(l < shortest->noNodes){
+        if(shortest->dist[l] == infinity){
+            shortest->dist[l] = 0;
+        }
+        l++;
+    }
     return *shortest;
 }
 
 
 
-void  showShortestPaths(ShortestPaths shortest){
-    //howwwwww toooooo showwwwwww??????
+void  showShortestPaths(ShortestPaths sps){
+    int i = 0;
+    printf("Node %d\n",sps.src);
+    printf("  Distance\n");
+    for (i = 0; i < sps.noNodes; i++) {
+        if(i == sps.src)
+            printf("    %d : X\n",i);
+        else
+            printf("    %d : %d\n",i,sps.dist[i]);
+    }
+    printf("  Preds\n");
+    for (i = 0; i < sps.noNodes; i++) {
+        printf("    %d : ",i);
+        PredNode* curr = sps.pred[i];
+        while(curr!=NULL) {
+            printf("[%d]->",curr->v);
+            curr = curr->next;
+        }
+        printf("NULL\n");
+    }
     
 }
 
 
 void  freeShortestPaths(ShortestPaths shortest){
     int i = 0;
+
     while(i < shortest.noNodes){
         PredNode* record = shortest.pred[i];
         PredNode* tempUse;
         while(record != NULL){
+            
             tempUse = record->next;
+            
             free(record);
+            
             record = tempUse;
         }
         i++;
--- PQ.c
+++ PQ.c
@@ -1,114 +1,128 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <assert.h>
 #include "PQ.h"
 
 #define MaxSlots 2048 //big enough?
 #define True 1
 #define False 0
 
-
-typedef struct ItemPQ *Item;   //Is the order right?? write it here is right?
-
+typedef struct ItemPQ *Item;   //Is the order right?? yes!
 //heap
 typedef struct PQRep{
-    Item *items;
+    Item* items;
     int nitems;
     int nslots;
 }PQRep;
 
 
-void swap(Item a,Item b);
-int checkOrSwap(Item a,Item b,Item c,int n);
+void swap(PQ pq,int a,int b);
+int checkOrSwap(PQ pq,int a,int b,int c);
 
-PQ newPQ(){
-    PQ new = calloc(1,sizeof(PQRep));
+PQ newPQ() {
+    PQ new = malloc(sizeof(PQRep));
+    assert(new != NULL);
     new->nslots = MaxSlots;
     new->nitems = 0;
-    new->items = calloc((MaxSlots+1),sizeof(Item));
-    
+    new->items  = malloc((MaxSlots+1)*sizeof(Item));
+    int i = 0;
+    while(i <= MaxSlots){
+        new->items[i] = malloc(sizeof(PQRep));
+        i++;
+    }
     return new;
 }
 
 void addPQ(PQ pq, ItemPQ im){
-    pq->nitems = pq->nitems+1;
-    // Is that right???  &&&????????
-    pq->items[pq->nitems] = &im;
+    pq->nitems++;
+    
+    *pq->items[pq->nitems] = im;
+
+  
     int n = pq->nitems;
-    while(n/2 >= 1){
-        if(pq->items[n]->value < pq->items[n/2]->value){
-            swap(pq->items[n],pq->items[n/2]);
-        }else{
-            break;
-        }
+
+
+    while (n > 1 && pq->items[n/2]->value > pq->items[n]->value) {
+        swap(pq, n, n/2);
         n = n/2;
+        
     }
+
 }
 
-void swap(Item a,Item b){
-    Item c = a;
-    a = b;
-    b = c;
+
+void swap(PQ pq, int a,int b){
+
+    Item c = pq->items[a];
+    pq->items[a] = pq->items[b];
+    pq->items[b] = c;
 }
+
 
 ItemPQ dequeuePQ(PQ pq){
-  
-    //will/Must not be empty?????
-    /*  if(pq->items[1] == NULL){
-        return NULL;
-    }
-   */
-   
-   //Is that right??
-    Item topOne = pq->items[1];
-    free(pq->items[1]);
-    //move the most bottom-right one to the top
-    pq->items[1] = pq->items[pq->nitems];
-    pq->items[pq->nitems] = NULL;
-    pq->nitems = pq->nitems-1;
+   // if (pq->nitems == 0) return NULL;
+  //  printf("n->items :%d\n",pq->nitems);
+   // printf("de\n");
+  //  printf("hereeee %d\n",pq->items[pq->nitems]->value);
+  //  printf("%d\n",pq->nitems);
+
+
+   // printf("print hereee");
+   // printf("thereeeeee %d\n",pq->items[1]->key);
     
-    //fix
+    swap(pq,1, pq->nitems);
+
+    pq->nitems--;
+    //printf("here\n");
+    //printf("%d\n",pq->items[pq->nitems+1]->key);
+    
     int n = 1;
-    while(2*n < pq->nitems){
-        //return the index of the child(left or right) swap with,or return false for not swap
-        n = checkOrSwap(pq->items[n],pq->items[2*n],pq->items[2*n+1],n);
+    while(2*n <= pq->nitems){
+        if((2*n+1) == pq->nitems+1){
+            if(pq->items[2*n]->value < pq->items[n]->value){
+                swap(pq, n, 2*n);
+            }
+            break;
+            
+        }
+        //return the index of the child(left or right) swap with,or return false for not swap.
+        n = checkOrSwap(pq,n,2*n,2*n+1);
         if(n == False){
             break;
         }
     }
-    return *topOne;
+    //printf("%d\n",pq->nitems);
+   // printf("thereee\n");
+ //   printf("%d\n",pq->items[pq->nitems+1]->key);
+    
+    
+    return *pq->items[pq->nitems+1];
 }
 
-int checkOrSwap(Item a,Item b,Item c,int n){
-    if(c != NULL){
-        if(a->value < b->value && a->value < c->value){
-            return False;
-        }else if(a->value > b->value && a->value < c->value){
-            swap(a,b);
-            return 2*n;
-        }else if(a->value < b->value && a->value > c->value){
-            swap(a,c);
-            return 2*n+1;
-        }else if(a->value > b->value && a->value > c->value){
-            if(b->value < c->value){
-                swap(a,b);
-                return 2*n;
-            }else{
-                swap(a,c);
-                return 2*n+1;
-            }
-        }
-    // for the case: c == NULL
-    }else{
-        if(a->value < b->value){
-            return False;
+
+int checkOrSwap(PQ pq,int a,int b,int c){
+    
+    if(pq->items[a]->value < pq->items[b]->value && pq->items[a]->value < pq->items[c]->value){
+        return False;
+    }else if(pq->items[a]->value > pq->items[b]->value && pq->items[a]->value < pq->items[c]->value){
+        swap(pq,a,b);
+        return b;
+    }else if(pq->items[a]->value < pq->items[b]->value && pq->items[a]->value > pq->items[c]->value){
+        swap(pq,a,c);
+        return c;
+    }else if(pq->items[a]->value > pq->items[b]->value && pq->items[a]->value > pq->items[c]->value){
+        if(pq->items[b]->value < pq->items[c]->value){
+            swap(pq,a,b);
+            return b;
         }else{
-            swap(a,b);
-            return 2*n;
+            swap(pq,a,c);
+            return c;
         }
     }
     return 0;
 }
 
+//have not checked!!!!!!!!!!
 void  updatePQ(PQ pq, ItemPQ im){
     int n = 1;
     while(n <= pq->nitems){
@@ -121,25 +135,26 @@ void  updatePQ(PQ pq, ItemPQ im){
 }
 
 int PQEmpty(PQ pq){
-    if(pq->items[1] == NULL){
+    if(pq->nitems == 0){
         return True;
-    }else{
-        return False;
     }
+    return False;
 }
+
 
 //is it the right way to show??? like a tree???
 void  showPQ(PQ pq){
-    int n = 1;
-    int record = 1;
-    while(n <= pq->nitems){
-        printf("%d",pq->items[n]->value);
-        n++;
-        if(n == record * 2){
-            printf("\n");
-            record = record * 2;
-        }
-    }
+//    int n = 1;
+//    int record = 1;
+//    while(n <= pq->nitems){
+//        printf("%d",pq->items[n]->value);
+//        n++;
+//        if(n == record * 2){
+//            printf("\n");
+//            record = record * 2;
+//        }
+//    }
+    printf("a\n");
 }
 
 void  freePQ(PQ pq){
@@ -154,9 +169,6 @@ void  freePQ(PQ pq){
     
     free(pq);
 }
-
-
-
 
 
 
