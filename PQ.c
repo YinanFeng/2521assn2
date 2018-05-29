a049d169832dfc6d5645b007cf84a556502bfd8b
z5123104 <30426880+z5123104@users.noreply.github.com>
2018-05-29 16:49:07 +1000

    with comments

--- PQ.c
+++ PQ.c
@@ -1,163 +1,169 @@
+// PQ.c 
+// taken from week12 lecture slides
+/* written by 
+        Haoyue Qiu(z5123104@unsw.edu.au)               */
+    
 #include<stdio.h>
 #include<stdlib.h>
 #include <assert.h>
 #include "PQ.h"
 
-#define MaxSlots 2048 //big enough?
+#define MaxSlots 2048 
 #define True 1
 #define False 0
 
-typedef struct ItemPQ *Item;   //Is the order right?? yes!
+typedef struct ItemPQ *Item;   
 //heap
 typedef struct PQRep{
-    Item items;
+    Item* items;
     int nitems;
     int nslots;
 }PQRep;
 
+int exist = 0; 
+// to find if the key has already existed in PQ
+void swap(PQ pq,int a,int b);
+// to swap PQ->items[a] and PQ->items[b]
+int checkOrSwap(PQ pq,int root,int left,int right);
+//return the index of the child(left or right) swap with,or return false for not swap.
 
-void swap(PQ pq, Item a,Item b);
-int checkOrSwap(Item a,Item b,Item c,int n);
+
 PQ newPQ() {
-    pQRep new = calloc(1, sizeof(pQRep));
+    PQ new = malloc(sizeof(PQRep));
     assert(new != NULL);
     new->nslots = MaxSlots;
     new->nitems = 0;
-    new->items  = calloc(MaxSlots+1, sizeof(ItemPQ));
+    new->items  = malloc((MaxSlots+1)*sizeof(Item));
+    // initialize each value within PQ
+    int i = 0;
+    while(i <= MaxSlots){
+        new->items[i] = malloc(sizeof(PQRep));
+        // allocate memory to items[] to be big enough to store all the paths
+        i++;
+    }
     return new;
 }
 
+
 void addPQ(PQ pq, ItemPQ im){
-    int exist = 0;
-    assert(pq != NULL && im != NULL);
-    for (int x = 0; x < pg->nitems ; x++) {
-        if (pg->items[x]->key == im->key) {
-            pg->items[x]->value = im->value;
-            exist = 1;
-            break;
-        }
+    if (exist == 1) {
+    // if the key was in PQ then just update the new value of the key
+        updatePQ(pq,im);
+        return;    
     }
-    if (exist == 0) {
-        pq->items[pq->nitems] = &im;
-        pq->nitems++;    
-    }
+    pq->nitems++;  
+    *pq->items[pq->nitems] = im;
+    // add new key into PQ
     int n = pq->nitems;
-    while (n > 1 && pq->items[n/2] < pq->items[n]) {
-        swap(pq, n, k/2);
-        n = n/2;
+    while (n > 1 && pq->items[n/2]->value > pq->items[n]->value) {
+        // if there is more than one key and its value is smaller than the middle node
+        swap(pq, n, n/2);
+        // keep swapping until the key with smallest is put as first node
+        n = n/2;      
     }
 }
 
 
-void swap(PQ pq, Item a,Item b){
+void swap(PQ pq, int a,int b){
 
     Item c = pq->items[a];
     pq->items[a] = pq->items[b];
     pq->items[b] = c;
+    // swap a and b
 }
 
-void neverViolate(ItemPQ a[], int i, int n) {
-    while ( 2*i <= n) {
-        int j = 2*i;
-        if (j < n  && a[j].value < a[j+1].value) j++;
-        if (a[i].value >= a[j]) swap(a[i], a[j]);
-        i = j;
-    }
-}
 
 ItemPQ dequeuePQ(PQ pq){
-    if (pq != NULL && pq->nitems == 0) return NULL;
-    swap(pq->items[1], pg->items[nitems]);
+    swap(pq,1, pq->nitems);
+    // swap the first node and the last node
     pq->nitems--;
-    neverViolate(pq, 1, pq->nitems);
-    return pq->items[q->nitems+1];
+    // decrease the number of nodes in PQ
+    int n = 1;
+    while(2*n <= pq->nitems){
+    // to reorder PQ
+        if((2*n+1) == pq->nitems+1){
+        // if n = nitems/2 which means the last two nodes may need to be swapped
+            if(pq->items[2*n]->value < pq->items[n]->value) swap(pq, n, 2*n);
+            break;
+        }
+        //return the index of the child(left or right) swap with,or return false for not swap.
+        n = checkOrSwap(pq,n,2*n,2*n+1);
+        if(n == False)  break;
+    }
+    return *pq->items[pq->nitems+1];
 }
 
 
-int checkOrSwap(Item a,Item b,Item c,int n){
-    if(c != NULL){
-        if(a->value < b->value && a->value < c->value){
-            return False;
-        }else if(a->value > b->value && a->value < c->value){
-            swap(a,b);
-            return 2*n;
-        }else if(a->value < b->value && a->value > c->value){
-            swap(a,c);
-            return 2*n+1;
-        }else if(a->value > b->value && a->value > c->value){
-            if(b->value < c->value){
-                swap(a,b);
-                return 2*n;
-            }else{
-                swap(a,c);
-                return 2*n+1;
-            }
-        }
-    // for the case: c == NULL
-    }else{
-        if(a->value < b->value){
-            return False;
+int checkOrSwap(PQ pq,int root,int left,int right){
+    
+    if(pq->items[root]->value < pq->items[left]->value && pq->items[root]->value < pq->items[right]->value){
+    // if root is smaller than both right and left child
+        return False;
+    // no need to swap
+    }else if(pq->items[root]->value > pq->items[left]->value && pq->items[root]->value < pq->items[right]->value){
+    // if right > root > left
+        swap(pq,root,left);
+        return left;
+    }else if(pq->items[root]->value < pq->items[left]->value && pq->items[root]->value > pq->items[right]->value){
+    // if left > root > right
+        swap(pq,root,right);
+        return right;
+    }else if(pq->items[root]->value > pq->items[left]->value && pq->items[root]->value > pq->items[right]->value){
+    // if root is the biggest one then find the smallest node between left and right
+        if(pq->items[left]->value < pq->items[right]->value){
+            swap(pq,root,left);
+            return left;
         }else{
-            swap(a,b);
-            return 2*n;
+            swap(pq,root,right);
+            return right;
         }
     }
     return 0;
 }
 
+
 void  updatePQ(PQ pq, ItemPQ im){
     int n = 1;
     while(n <= pq->nitems){
         if(pq->items[n]->key == im.key){
+        // if key alrady existed in PQ
             pq->items[n]->value = im.value;
             break;
         }
         n++;
     }
+    exist = 1;
 }
 
 int PQEmpty(PQ pq){
-    if(pq->items[1] == NULL){
+    if(pq->nitems == 0){
         return True;
-    }else{
-        return False;
     }
+    return False;
 }
+
 
 //is it the right way to show??? like a tree???
 void  showPQ(PQ pq){
-    int n = 1;
-    int record = 1;
-    while(n <= pq->nitems){
-        printf("%d",pq->items[n]->value);
-        n++;
-        if(n == record * 2){
-            printf("\n");
-            record = record * 2;
-        }
-    }
+//    int n = 1;
+//    int record = 1;
+//    while(n <= pq->nitems){
+//        printf("%d",pq->items[n]->value);
+//        n++;
+//        if(n == record * 2){
+//            printf("\n");
+//            record = record * 2;
+//        }
+//    }
+    printf("a\n");
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void  freePQ(PQ pq){
+    int n = 1;
+    while(n <= pq->nitems){
+        free(pq->items[n]);
+        n++;
+    }
+    free(pq->items);
+    free(pq);
+}
